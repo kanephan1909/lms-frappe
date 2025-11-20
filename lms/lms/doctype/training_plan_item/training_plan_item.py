a12a747eb380c7d5c85d13170707f7e197672e3e
plnkhang <pkhang962@gmail.com>
2025-11-20 10:31:07 +0700

    Add Training Plan Item DocType with fields for course, role, training mode, and cost

---
+++ lms/lms/doctype/training_plan_item/training_plan_item.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Frappe and contributors
+# For license information, please see license.txt
+
+from frappe.model.document import Document
+
+
+class TrainingPlanItem(Document):
+	pass
+
