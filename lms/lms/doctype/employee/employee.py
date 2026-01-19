0f53deca6e14412fb356816441fad11b894e6385
plnkhang <pkhang962@gmail.com>
2026-01-19 13:55:14 +0700

    Remove obsolete doctype files and associated code for Designation, Employee, Training Assignment, Training Period, Training Plan, and Training Plan Item. This includes deletion of Python files, JSON configurations, and JavaScript files to streamline the LMS module.

--- lms/lms/doctype/employee/employee.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2025, Frappe and contributors
-# For license information, please see license.txt
-
-from frappe.model.document import Document
-
-
-class Employee(Document):
-	pass
-
